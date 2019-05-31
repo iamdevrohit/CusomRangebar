b10e5c8cfc82796f448be0cdfca9e7b6f7e3d4d4
blackmamba <rvworkstation@gmail.com>
2019-05-31 14:29:00 +0530

    initial commit

--- app/src/main/java/com/example/librerytest/MainActivity.java
+++ app/src/main/java/com/example/librerytest/MainActivity.java
@@ -2,19 +2,15 @@ package com.example.librerytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.blackmambalibrary.Mymath;
+import com.example.blackmambalibrary.interfaces.OnRangeSeekbarChangeListener;
+import com.example.blackmambalibrary.widgets.BlackmambaRangebar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    EditText secondvalue,etfirstvalue;
-    Button btnplus,btnminus,btnmultiply,btndevide;
-    TextView tvresult;
-
+    BlackmambaRangebar blackmambaRangebar;
+    TextView tvstartvalue,tvstopvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,54 +18,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         bindViews();
 
-        //clickListner
-        btndevide.setOnClickListener(this);
-        btnminus.setOnClickListener(this);
-        btnmultiply.setOnClickListener(this);
-        btnplus.setOnClickListener(this);
-    }
+        blackmambaRangebar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
 
+                tvstartvalue.setText(String.valueOf(minValue));
+                tvstopvalue.setText(String.valueOf(maxValue));
+            }
+        });
+
+    }
     private void bindViews() {
+        blackmambaRangebar =findViewById(R.id.rangeseekbar);
+        tvstartvalue=findViewById(R.id.tvstartvalue);
+        tvstopvalue=findViewById(R.id.tvstopvalue);
 
-        secondvalue=findViewById(R.id.secondvalue);
-        etfirstvalue=findViewById(R.id.etfirstvalue);
-        btnplus=findViewById(R.id.btnplus);
-        btnminus=findViewById(R.id.btnminus);
-        btnmultiply=findViewById(R.id.btnmultiply);
-        btndevide=findViewById(R.id.btndevide);
-        tvresult=findViewById(R.id.tvresult);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnplus:
-
-                int a= Integer.parseInt(etfirstvalue.getText().toString());
-                int b= Integer.parseInt(secondvalue.getText().toString());
-                tvresult.setText(String.valueOf(Mymath.plus(a,b)));
-                break;
-
-            case R.id.btnminus:
-
-                int a1= Integer.parseInt(etfirstvalue.getText().toString());
-                int b1= Integer.parseInt(secondvalue.getText().toString());
-                tvresult.setText(String.valueOf(Mymath.minus(a1,b1)));
-                break;
-
-            case R.id.btnmultiply:
-
-                int a2= Integer.parseInt(etfirstvalue.getText().toString());
-                int b2= Integer.parseInt(secondvalue.getText().toString());
-                tvresult.setText(String.valueOf(Mymath.multiply(a2,b2)));
-                break;
-
-            case R.id.btndevide:
-
-                int a3= Integer.parseInt(etfirstvalue.getText().toString());
-                int b3= Integer.parseInt(secondvalue.getText().toString());
-                tvresult.setText(String.valueOf(Mymath.devide(a3,b3)));
-                break;
-        }
-    }
 }
